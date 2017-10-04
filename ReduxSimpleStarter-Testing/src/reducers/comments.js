0f79ff9f9145081d9c24c7ce308d00f0253d11e5
Unknown <steven.chou.second@gmail.com>
2017-10-04 21:31:57 +0800

    App Review

--- ReduxSimpleStarter-Testing/src/reducers/comments.js
+++ ReduxSimpleStarter-Testing/src/reducers/comments.js
@@ -1,7 +1,6 @@
 import { SAVE_COMMENT } from './../actions/types';
 
 export default function(state = [], action) {
-  console.log('@@ state', state);
   switch (action.type) {
     case SAVE_COMMENT:
       // return state.concat([action.payload])
