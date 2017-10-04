7a58a227421809b2bade3060542a9e75c971e8b3
Unknown <steven.chou.second@gmail.com>
2017-10-04 15:58:13 +0800

    Using beforeEach to Condense Tests

--- ReduxSimpleStarter-Testing/test/components/comment_box_test.js
+++ ReduxSimpleStarter-Testing/test/components/comment_box_test.js
@@ -1,22 +1,24 @@
 import { renderComponent, expect } from './../test_helper';
 import CommentBox from './../../src/components/comment_box';
 
-describe('CommentBox',() => {
-    it('has a correct class', () => {
-        // actually return jquery obj
-        const component = renderComponent(CommentBox);
-        expect(component).to.have.class('comment-box');
-    })
+describe('CommentBox', () => {
+  let component;
+  // run before any of our statement and it's going to run before each of them runs.
+  // 所以總共呼叫 3 次
+  beforeEach(() => {
+    // actually return jquery obj
+    component = renderComponent(CommentBox);
+  });
 
-    it('has a text area', () => {
-        // actually return jquery obj
-        const component = renderComponent(CommentBox);
-        expect(component.find('textarea')).to.exit;
-    });
+  it('has a correct class', () => {
+    expect(component).to.have.class('comment-box');
+  });
 
-    it('has a button', () => {
-        // actually return jquery obj
-        const component = renderComponent(CommentBox);
-        expect(component.find('textarea')).to.exit;
-    });
+  it('has a text area', () => {
+    expect(component.find('textarea')).to.exit;
+  });
+
+  it('has a button', () => {
+    expect(component.find('textarea')).to.exit;
+  });
 });
