b60e9c4ec74031a59f758f524943669e00bc70b3
Unknown <steven.chou.second@gmail.com>
2017-10-04 16:25:09 +0800

    Expecting Child Elements

--- ReduxSimpleStarter-Testing/test/components/app_test.js
+++ ReduxSimpleStarter-Testing/test/components/app_test.js
@@ -3,6 +3,7 @@ import App from './../../src/components/app';
 
 // Use 'describe' to group together similar tests
 describe('App', () => {
+  /* 
   // Use 'it' to test a single attribute of a target
   it('show the correct text', () => {
     // create an instance of App
@@ -10,5 +11,16 @@ describe('App', () => {
 
     // Use 'expect' to make an 'assertion' about a target
     expect(component).to.contain('React simple starter');
+  });
+   */
+
+  let component;
+
+  beforeEach(() => {
+    component = renderComponent(App);
+  });
+
+  it('shows a comment box', () => {
+    expect(component.find('.comment-box')).to.exist;
   });
 });
