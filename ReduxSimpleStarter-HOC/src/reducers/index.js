6e18fb0db202d01d0094edff778a5f736b92ff20
Unknown <steven.chou.second@gmail.com>
2017-10-11 15:17:08 +0800

    Authentication Reducer

--- ReduxSimpleStarter-HOC/src/reducers/index.js
+++ ReduxSimpleStarter-HOC/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import authenticationReducer from './authenticate';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  // state: (state = {}) => state
+  authenticated: authenticationReducer
 });
 
 export default rootReducer;
