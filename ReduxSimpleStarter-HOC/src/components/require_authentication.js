bd9dcd41b6603cf1d3987f65b189213953b31de6
Unknown <steven.chou.second@gmail.com>
2017-10-11 16:27:52 +0800

    HOC Scaffold Code

--- ReduxSimpleStarter-HOC/src/components/require_authentication.js
+++ ReduxSimpleStarter-HOC/src/components/require_authentication.js
@@ -3,9 +3,24 @@ import React, { Component } from 'react';
 export default function(ComposedComponent) {
   class Authentication extends Component {
     render() {
+     /*  
+     console.log(this.props.resources); // => resourceList
+      */
       return <ComposedComponent {...this.props} />;
     }
   }
 
   return Authentication;
 }
+
+/* 
+    // In some other location... Not in this file...
+    // We want to use this HOC
+    import Authentication; // This is my HOC
+    import Resources; // This is the component I want to wrap
+
+    const ComposedComponent = Authentication(Resources);
+
+    // In some render method...
+    <ComposedComponent resources={resourceList}/>
+*/
