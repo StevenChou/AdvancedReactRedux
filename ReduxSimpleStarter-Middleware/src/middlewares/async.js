de713ea340aa44ae98f2db26fd2f3483dce7675a
Unknown <steven.chou.second@gmail.com>
2017-10-22 17:20:06 +0800

    Handling Promises

--- ReduxSimpleStarter-Middleware/src/middlewares/async.js
+++ ReduxSimpleStarter-Middleware/src/middlewares/async.js
@@ -1,13 +1,19 @@
-export default function({dispatch}) {
-    return next => action => {
-        // If action does not have payload
-        // or, the payload does not have a .then property
-        // we don't care about it, send it on
-        if(!action.payload || !action.payload.then) {
-            next(action);
-        }
-
-        // call next middleware or reducers
-        next(action);
+export default function({ dispatch }) {
+  return next => action => {
+    // If action does not have payload
+    // or, the payload does not have a .then property
+    // we don't care about it, send it on
+    if (!action.payload || !action.payload.then) {
+      // call next middleware or reducers
+      next(action);
     }
+
+    // Make sure that action's promise resolves
+    action.payload.then(function(response) {
+      // create a new action with the old type, but
+      // replace the promise with the response data
+      const newAction = { ...action, payload: response };
+      dispatch(newAction);
+    });
+  };
 }
