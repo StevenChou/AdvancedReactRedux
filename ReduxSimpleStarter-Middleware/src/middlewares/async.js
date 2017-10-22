890441c0411e4b2d51ea92e3000b17384dd15419
Unknown <steven.chou.second@gmail.com>
2017-10-22 16:35:13 +0800

    Handling Unrelated Actions

--- ReduxSimpleStarter-Middleware/src/middlewares/async.js
+++ ReduxSimpleStarter-Middleware/src/middlewares/async.js
@@ -1,6 +1,11 @@
 export default function({dispatch}) {
     return next => action => {
-        console.log(action);
+        // If action does not have payload
+        // or, the payload does not have a .then property
+        // we don't care about it, send it on
+        if(!action.payload || !action.payload.then) {
+            next(action);
+        }
 
         // call next middleware or reducers
         next(action);
