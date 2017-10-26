814f23abdcc9301c727049985945f5caf90ecd18
Unknown <steven.chou.second@gmail.com>
2017-10-26 21:58:48 +0800

    偵測 MongoDB

--- server/index.js
+++ server/index.js
@@ -3,14 +3,18 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 const app = express();
 
 const router = require('./router');
 
+// DB Setup
+mongoose.connect('mongodb://localhost:auth/auth'); // creates a new database [auth] inside of mongoDB
+
 // App Setup
 // use 即註冊 middleware
 app.use(morgan('combined')); // logging framework[logging incoming request]
-app.use(bodyParser.json({ type: '*.*' })); // parse any incoming request
+app.use(bodyParser.json({ type: '*/*' })); // parse any incoming request
 router(app);
 
 //
