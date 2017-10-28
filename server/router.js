d9641d30ec13a5edabe5572f7049991d769db7ac
Unknown <steven.chou.second@gmail.com>
2017-10-28 15:47:40 +0800

    Making an Authenticated Request

--- server/router.js
+++ server/router.js
@@ -1,4 +1,9 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+// try to make cookie based session
+const requireAuth = passport.authenticate('jwt', { session: false });
 
 module.exports = function(app) {
   app.post('/signup', Authentication.signup);
