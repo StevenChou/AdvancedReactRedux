3a9ba3cf42dedc8e10750aecb6b39c505250af1b
Unknown <steven.chou.second@gmail.com>
2017-10-28 16:52:58 +0800

    Bcrypt Full Circle

--- server/models/user.js
+++ server/models/user.js
@@ -36,6 +36,17 @@ userSchema.pre('save', function(next) {
   });
 });
 
+// 在 userSchema 增加一個 method
+userSchema.methods.comparePassword = function(candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) {
+      return callback(err);
+    }
+
+    callback(null, isMatch);
+  });
+};
+
 // Create the model class
 const ModelClass = mongoose.model('user', userSchema); // user is table name
 
