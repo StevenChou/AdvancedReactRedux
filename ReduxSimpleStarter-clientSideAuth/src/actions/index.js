5f8df5d7114e2eda3db822ffd8557421893f0d50
Unknown <steven.chou.second@gmail.com>
2017-11-05 15:27:25 +0800

    Action Creator with Many Responsibilities

---
+++ ReduxSimpleStarter-clientSideAuth/src/actions/index.js
@@ -0,0 +1,12 @@
+
+
+export function signinUser({email, password}) {
+    // Submit email/password to the server
+
+    // If request is good...
+    // - Update state to indicate user is authenticated
+    // - redirect to router '/feature'
+    
+    // If request is bad...
+    // - Show an error to the user
+}
