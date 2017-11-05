91c07368eaa1a2cbceb13735e74dea68c2971d12
Unknown <steven.chou.second@gmail.com>
2017-11-05 14:36:35 +0800

    Scaffolding the Signin Form

--- ReduxSimpleStarter-clientSideAuth/src/reducers/index.js
+++ ReduxSimpleStarter-clientSideAuth/src/reducers/index.js
@@ -1,7 +1,13 @@
 import { combineReducers } from 'redux';
+import { reducer as form } from 'redux-form';
 
+// const rootReducer = combineReducers({
+//   form: form
+// });
+
+// ES6
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  form
 });
 
 export default rootReducer;
