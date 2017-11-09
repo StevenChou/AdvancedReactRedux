7df2292d897346a37a52dc9fb1ac977be037e761
Unknown <steven.chou.second@gmail.com>
2017-11-09 17:40:55 +0800

    Implementing Validation Logic

--- ReduxSimpleStarter-clientSideAuth/src/components/auth/signup.js
+++ ReduxSimpleStarter-clientSideAuth/src/components/auth/signup.js
@@ -20,19 +20,39 @@ class Signup extends Component {
         </fieldset>
         <fieldset className="form-group">
           <label>Password:</label>
-          <input className="form-control" {...password} type="password"/>
+          <input className="form-control" {...password} type="password" />
+          {/* cool condition */}
+          {password.touched && password.error && <div className="error">{password.error}</div>}
         </fieldset>
         <fieldset className="form-group">
           <label>Confirm Password:</label>
-          <input className="form-control" {...passwordConfirm} type="password"/>
+          <input
+            className="form-control"
+            {...passwordConfirm}
+            type="password"
+          />
         </fieldset>
-        <button className="btn btn-primary" type="submit">Sign up!</button>
+        <button className="btn btn-primary" type="submit">
+          Sign up!
+        </button>
       </form>
     );
   }
 }
 
+function validate(formProps) {
+  console.log('@@formProps', formProps);
+  const errors = {};
+
+  if (formProps.password !== formProps.passwordConfirm) {
+    errors.password = 'Passwords must match';
+  }
+
+  return errors;
+}
+
 export default reduxForm({
   form: 'signup',
-  fields: ['email', 'password', 'passwordConfirm']
+  fields: ['email', 'password', 'passwordConfirm'],
+  validate: validate
 })(Signup);
