6e3d428684c68b63f56b24cacb856d1e13d8c239
Unknown <steven.chou.second@gmail.com>
2017-11-11 14:13:38 +0800

    Securing Individual Routes

---
+++ ReduxSimpleStarter-clientSideAuth/src/components/auth/require_auth.js
@@ -0,0 +1,34 @@
+// HOC ^^
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default function(ComposedComponent) {
+  class Authentication extends Component {
+    static contextTypes = {
+      router: React.PropTypes.object
+    };
+
+    componentWillMount() {
+      if (!this.props.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    // 呼叫時機
+    componentWillUpdate(nextProps) {
+      if (!nextProps.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    render() {
+      return <ComposedComponent {...this.props} />;
+    }
+  }
+
+  function mapStateToProps(state) {
+    return { authenticated: state.auth.authentication };
+  }
+
+  return connect(mapStateToProps)(Authentication);
+}
